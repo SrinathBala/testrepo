35c33cba4fd97b44c593ca4f459272a964e09bf5
SrinathBala <srinathbala00@gmail.com>
2022-09-05 10:13:16 +0530

    ec2

--- lib/ec2-stack.ts
+++ lib/ec2-stack.ts
@@ -1,16 +1,13 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class Ec2Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Ec2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+    const s3Bucket = new s3.Bucket(this, 'exampleBucket', {
+    });  }
 }
